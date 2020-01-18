5cd6474fc44a07d39a4ffbd777fb2bc7cdb3555e
Rohit <rohitbulusu@gmail.com>
2020-01-17 22:53:49 -0500

    deleted default react files

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img alt="photo of great jagras" src=""></img>
+      <div>
+        <h2>Great Jagras</h2>
+        <p>Poor monster just hungry</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
